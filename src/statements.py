f0a720176b13ae34fdaf8a359dfb24b9220decf3
Hossain Eshan <eshan.shahriar.77@gmail.com>
2025-08-14 19:22:27 -0400

    Implemented dataclass for create table, with a Column class to be expanded later

--- src/statements.py
+++ src/statements.py
@@ -1,20 +1,37 @@
 from dataclasses import dataclass
+from typing import Any, List, Optional, Type
+
+
+@dataclass
+class Column:
+    name: str
+    type: Type
+    nullable: bool
+    default: Optional[Any]
+    primary_key: bool
+    unique: bool
+
 
 @dataclass
 class UpdateTableStatement:
     pass
 
+
 @dataclass
 class CreateTableStatement:
-    pass
+    table_name: str
+    columns: List[Column]
+
 
 @dataclass
 class DropTableStatement:
     pass
 
+
 @dataclass
 class SelectStatement:
     pass
+
 
 @dataclass
 class InsertStatement:
