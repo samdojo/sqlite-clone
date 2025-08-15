4c8ea4d450b312786df8921e650b298c35b2fee5
Hossain Eshan <eshan.shahriar.77@gmail.com>
2025-08-15 18:24:17 -0400

    Changes for statements

--- src/statements.py
+++ src/statements.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import TypeAlias
+from typing import Optional, Self, TypeAlias, Union
 
 
 @dataclass
@@ -36,3 +36,20 @@ class Literal:
 
     dtype: type
     value: LiteralType
+
+
+@dataclass
+class Table:
+    """Container for table name, schema name and alias."""
+
+    table_name: str
+    schema_name: Optional[str]
+    alias: Optional[str]
+
+
+@dataclass
+class SubQuery:
+    """Container for subquery."""
+
+    content: Union[SelectStatement, Self]
+    alias: Optional[str] = None
