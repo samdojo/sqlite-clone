fed8a0364130e3a709e7d9391f48887cd2ab344b
H S Eshan <eshan.shahriar.77@gmail.com>
2025-08-22 12:44:28 -0400

    Created column name list parser, added unit tests (#7)
    * Created column name list parser, added unit tests
    
    * Updated logic to check beginning of tokens

---
+++ src/test-columnnamelistparser.py
@@ -0,0 +1,57 @@
+import pytest
+
+from baseparser import ParsingException
+from columnnamelistparser import ColumnNameListParser
+from sqltokenizer import Tokenizer
+
+
+class TestColumnNameListParser:
+    def setup_method(self):
+        self.tokenizer = Tokenizer()
+
+    def test_parse_column_name_list(self):
+        tokens = self.tokenizer.tokenize("(column1, column2, column3)")
+        print(tokens)
+        parser = ColumnNameListParser(tokens)
+        result = parser.parse()
+        assert result == ["column1", "column2", "column3"]
+
+    def test_parse_missing_comma(self):
+        tokens = self.tokenizer.tokenize("(column1 column2 column3")
+        parser = ColumnNameListParser(tokens)
+        with pytest.raises(ParsingException) as e:
+            parser.parse()
+        assert (
+            e.value.args[0]
+            == "Unexpected token after column name, got Token(type=<TokenType.IDENTIFIER: 'IDENTIFIER'>, value='column2'). Expected comma or parenthesis."
+        )
+
+    def test_missing_column_name(self):
+        tokens = self.tokenizer.tokenize("(column1, column2,)")
+        parser = ColumnNameListParser(tokens)
+        with pytest.raises(ParsingException) as e:
+            parser.parse()
+        assert (
+            e.value.args[0]
+            == "Unexpected token after comma, got Token(type=<TokenType.RPAREN: 'RPAREN'>, value=')')"
+        )
+
+    def test_missing_rparen(self):
+        tokens = self.tokenizer.tokenize("(column1, column2")
+        parser = ColumnNameListParser(tokens)
+        with pytest.raises(ParsingException) as e:
+            parser.parse()
+        assert (
+            e.value.args[0]
+            == "Unexpected token after column name, got Token(type=<TokenType.EOF: 'EOF'>, value=''). Expected comma or parenthesis."
+        )
+
+    def test_inner_parentheses(self):
+        tokens = self.tokenizer.tokenize("(column1, (column2))")
+        parser = ColumnNameListParser(tokens)
+        with pytest.raises(ParsingException) as e:
+            parser.parse()
+        assert (
+            e.value.args[0]
+            == "Unexpected token after comma, got Token(type=<TokenType.LPAREN: 'LPAREN'>, value='(')"
+        )
