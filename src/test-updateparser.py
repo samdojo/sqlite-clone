1b6ee67c8cff26a7bacf470ccc5dd515b761cd5a
dialectician4 <142185883+dialectician4@users.noreply.github.com>
2025-09-23 11:24:08 -0400

    Ft/update exec (#24)
    * Setting up work for update executor
    
    * Completed updateexecutor with tests. Made some changes to updateparser files, added new functionality to data.Table, and fixed some bugs causing failed tests.

--- src/test-updateparser.py
+++ src/test-updateparser.py
@@ -1,59 +1,41 @@
 import unittest
 from sqltoken import Token, TokenType
+from sqltokenizer import Tokenizer
 from updateparser import UpdateParser
 from baseparser import ParsingException
 from statements import UpdateStatement
 
+
 class TestUpdateParser(unittest.TestCase):
 
     def test_basic_update_statement(self):
-        tokens = [
-            Token("UPDATE", TokenType.KEYWORD),
-            Token("users", TokenType.IDENTIFIER),
-            Token("SET", TokenType.KEYWORD),
-            Token("name", TokenType.IDENTIFIER),
-            Token("=", TokenType.OPERATOR),
-            Token("'Alice'", TokenType.STRING_LITERAL),
-            Token("WHERE", TokenType.KEYWORD),
-            Token("id", TokenType.IDENTIFIER),
-            Token("=", TokenType.OPERATOR),
-            Token("1", TokenType.NUMBER_LITERAL),
-        ]
+        t = Tokenizer()
+        tokens = t.tokenize("UPDATE users SET name = 'Alice' WHERE id = 1")
         parser = UpdateParser(tokens)
         statement = parser.parse()
 
         self.assertIsInstance(statement, UpdateStatement)
-        self.assertEqual(statement.table.name, "users")
+        self.assertEqual(statement.table.table_name, "users")
         self.assertEqual(len(statement.set_assignments), 1)
         self.assertEqual(statement.set_assignments[0]['columns'][0], "name")
-        self.assertEqual(statement.set_assignments[0]['expression'].value, "'Alice'")
-        self.assertEqual(statement.where_expr.left.value, "id")
-        self.assertEqual(statement.where_expr.right.value, "1")
+        self.assertEqual(statement.set_assignments[0]['expression'].lead_expr.value, "Alice")
+        self.assertEqual(statement.where_expr.lead_expr.lead_expr.column_name, "id")
+        self.assertEqual(statement.where_expr.second_expr.lead_expr.value, 1)
         self.assertIsNone(statement.from_clause)
         self.assertIsNone(statement.returning_exprs)
         self.assertIsNone(statement.or_action)
 
     def test_update_with_multiple_set_assignments(self):
-        tokens = [
-            Token("UPDATE", TokenType.KEYWORD),
-            Token("products", TokenType.IDENTIFIER),
-            Token("SET", TokenType.KEYWORD),
-            Token("price", TokenType.IDENTIFIER),
-            Token("=", TokenType.OPERATOR),
-            Token("99.99", TokenType.NUMBER_LITERAL),
-            Token(",", TokenType.COMMA),
-            Token("stock", TokenType.IDENTIFIER),
-            Token("=", TokenType.OPERATOR),
-            Token("100", TokenType.NUMBER_LITERAL),
-        ]
+        t = Tokenizer()
+        tokens = t.tokenize("UPDATE products SET price = 99.99, stock = 100")
         parser = UpdateParser(tokens)
         statement = parser.parse()
 
         self.assertEqual(len(statement.set_assignments), 2)
         self.assertEqual(statement.set_assignments[0]['columns'][0], "price")
-        self.assertEqual(statement.set_assignments[0]['expression'].value, "99.99")
+        self.assertEqual(statement.set_assignments[0]['expression'].lead_expr.value, 99.99)
         self.assertEqual(statement.set_assignments[1]['columns'][0], "stock")
-        self.assertEqual(statement.set_assignments[1]['expression'].value, "100")
+        self.assertEqual(statement.set_assignments[1]['expression'].lead_expr.value, 100)
 
 
 if __name__ == '__main__':
